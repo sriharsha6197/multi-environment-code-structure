86721138e93fbccdfaa80bdb8ad6205c51628f46
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-03 17:02:19 +0530

    c

--- code-structure/modules/vpc/main.tf
+++ code-structure/modules/vpc/main.tf
@@ -38,6 +38,7 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 resource "aws_route_table_association" "public_route_table_association" {
+  count = length(aws_subnet.public_subnets)
   subnet_id = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public_route_table[count.index].id
 }
