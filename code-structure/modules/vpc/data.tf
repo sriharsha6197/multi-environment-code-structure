4e7188d05f5aff186e2c6df597aa354df5a4e3f2
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-03 18:31:33 +0530

    sec group creation

--- code-structure/modules/vpc/data.tf
+++ code-structure/modules/vpc/data.tf
@@ -3,4 +3,9 @@ data "aws_ami" "ami" {
   name_regex = "Centos-8-DevOps-Practice"
   owners = [973714476881]
 }
-
+data "aws_security_group" "id_sg"{
+  id = var.security_group_id
+}
+variable "security_group_id" {
+  default = "sg-0139220bdae9b38db"
+}
