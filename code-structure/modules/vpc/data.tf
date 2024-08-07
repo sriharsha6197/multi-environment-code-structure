af31f00f1cbba14a96c625bda13154c4d6027ef8
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-07 22:54:15 +0530

    commenting out the code in data.tf file

--- code-structure/modules/vpc/data.tf
+++ code-structure/modules/vpc/data.tf
@@ -1,11 +1,11 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-  owners = [973714476881]
-}
-data "aws_security_group" "id_sg"{
-  id = var.security_group_id
-}
-variable "security_group_id" {
-  default = "sg-0139220bdae9b38db"
-}
+# data "aws_ami" "ami" {
+#   most_recent = true
+#   name_regex = "Centos-8-DevOps-Practice"
+#   owners = [973714476881]
+# }
+# data "aws_security_group" "id_sg"{
+#   id = var.security_group_id
+# }
+# variable "security_group_id" {
+#   default = "sg-0139220bdae9b38db"
+# }
