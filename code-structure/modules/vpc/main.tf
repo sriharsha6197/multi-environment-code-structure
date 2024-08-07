ad75dc798c2b55f8add759332fc1891c4ceb58fe
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-07 23:28:38 +0530

    peering

--- code-structure/modules/vpc/main.tf
+++ code-structure/modules/vpc/main.tf
@@ -107,7 +107,6 @@ resource "aws_vpc_peering_connection" "foo" {
   peer_owner_id = var.peer_owner_id
   peer_vpc_id   = var.peer_vpc_id
   vpc_id        = var.vpc_id
-  peer_region = "us-east-1"
   auto_accept   = true
 
   tags = {
